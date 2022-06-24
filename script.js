5b20a87674613be8e91da8d4382b0fc3156f41c9
Stefanita Plesa <plesa.stefanita@yahoo.ro>
2022-06-24 17:04:57 +0300

    add functionality

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-
-
-
 let wheatherKey = config.myApiKey;
+const output = document.getElementById("cities");
 
 const data = null;
 
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
+function searchWeather() {
+	const city = document.getElementById("searchInput");
 
-xhr.open("GET", "https://weatherapi-com.p.rapidapi.com/current.json?q=Braila");
-xhr.setRequestHeader("X-RapidAPI-Key", wheatherKey);
-xhr.setRequestHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com");
+	xhr.addEventListener("readystatechange", function () {
+		if (this.readyState === this.DONE) {
+			let test = JSON.parse(this.responseText);
+			
+			console.log(test);
 
-xhr.send(data);
+			output.innerHTML = `<li>The weather in ${test.location.name},${test.location.country}</li>`;
+			output.innerHTML += `<li>Last update at: ${test.location.localtime}</li>`;
+			output.innerHTML += `<li>${test.current.temp_c}°C</li>`;
+			output.innerHTML += `<li>Humidity of ${test.current.humidity}%</li>`;
+			
+			
+			
 
-console.log(data);
+
+		}
+	});
+
+
+
+	xhr.open("GET", `https://weatherapi-com.p.rapidapi.com/current.json?q=${city.value}`);
+	xhr.setRequestHeader("X-RapidAPI-Key", wheatherKey);
+	xhr.setRequestHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com");
+
+	xhr.send(data);
+}
+
+
+
+
+
+
 
